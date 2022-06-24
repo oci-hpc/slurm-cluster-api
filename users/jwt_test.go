082f85188c49ea484e2903c06cc099c58070b03f
Matthew Peddle <matthew.t.peddle@gmail.com>
2022-06-24 19:06:51 +0000

    unit test refresh token

--- users/jwt_test.go
+++ users/jwt_test.go
@@ -66,3 +66,66 @@ func TestValidateJWTToken(t *testing.T) {
 		})
 	}
 }
+
+func TestRefreshJWTToken(t *testing.T) {
+	userInfo := UserInfo{
+		Username: "TestUsername",
+		Email:    "test@test.com",
+	}
+	validTokenString, validRefreshTokenString, _ := GenerateJWTToken(userInfo)
+	type args struct {
+		tokenString        string
+		refreshTokenString string
+	}
+	tests := []struct {
+		name             string
+		args             args
+		wantToken        string
+		wantRefreshToken string
+		wantErr          bool
+	}{
+		{
+			name: "Valid Refresh Token",
+			args: args{
+				tokenString:        validTokenString,
+				refreshTokenString: validRefreshTokenString,
+			},
+			wantToken:        validTokenString,
+			wantRefreshToken: validRefreshTokenString,
+			wantErr:          false,
+		},
+		{
+			name: "Invalid Refresh Token",
+			args: args{
+				tokenString:        "badtoken",
+				refreshTokenString: "badtoken",
+			},
+			wantToken:        "",
+			wantRefreshToken: "",
+			wantErr:          true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			token, refreshToken, err := RefreshJWTToken(tt.args.refreshTokenString, userInfo)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RefreshJWTToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			//validate token
+			_, err = ValidateJWTToken(token)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWTToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			//validate refresh token
+			_, err = ValidateJWTToken(refreshToken)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWTToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
